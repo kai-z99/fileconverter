e9f78e5c981bc91d5480baa33ae070912288378b
kai-z99 <kai1241@yahoo.com>
2024-05-31 01:28:48 -0700

    qol changes

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from pydub import AudioSegment
 from PIL import Image
+import os
 ####################
 #AUDIO
 ####################
 
-def ConvertAudioFile(input_file_location, output_file_location, output_format):
+def ConvertAudioFile(input_file_location, output_file_location):
+    output_file_extension = os.path.splitext(output_file_location)[1]
     audio = AudioSegment.from_file(input_file_location)
-    audio.export(output_file_location, output_format)
+    audio.export(output_file_location, output_file_extension.strip("."))
     print(f"File converted and saved as {output_file}")
 
 input_file = "HighPressure_Music.mp3" 
-output_file = "HighPressure_Music.wav"  
-output_format = "wav" 
+output_file = "HighPressure_Music.ogg"  
 
-#ConvertAudioFile(input_file, output_file, output_format)
+ConvertAudioFile(input_file, output_file)
 
 
 
@@ -21,14 +22,22 @@ output_format = "wav"
 #IMAGE
 ####################
 def ConvertImageFile(input_file_location, output_file_location):
+    input_file_extension = os.path.splitext(input_file_location)[1]
+    output_file_extension = os.path.splitext(output_file_location)[1]
+
     with Image.open(input_file_location) as img:
-        img = img.convert("RGB")
+        if input_file_extension == output_file_extension:
+            print(f"error: input and output are both type {input_file_extension}.")
+            return
+        if output_file_extension == ".jpg":
+            img = img.convert("RGB")
+
         img.save(output_file_location, quality=100)
 
     print(f"File converted and saved as {output_file_location}")
 
-input_file = "spite.png" 
-output_file = "spite_converted.jpg"  
+input_file = "Rotating_earth_(large).png" 
+output_file = "Rotating_earth_(large).gif"  
 
 #ConvertImageFile(input_file, output_file)
 
